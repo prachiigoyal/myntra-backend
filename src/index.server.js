ab7b25dcd1c446065cb9f94f625ed7a3b2975024
Juveria Manzar <juveriamanzar29@gmail.com>
2021-04-06 10:21:49 +0530

    Update index.server.js

--- src/index.server.js
+++ src/index.server.js
@@ -40,6 +40,10 @@ mongoose
 // app.use(cors());
 
 app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://hackerramp-myntra-frontend.herokuapp.com/"
+  );
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
